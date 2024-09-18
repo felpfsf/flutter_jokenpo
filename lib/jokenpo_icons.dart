3fd96f0f015b1a9e32cd9f55709635248b4b4601
Felipe Ferreira <feferreira.sf@gmail.com>
2024-09-18 18:02:49 -0300

    🍱 icons

---
+++ lib/jokenpo_icons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class JokenpoIcons {
+  JokenpoIcons._();
+
+  static const _kFontFam = 'JokenpoIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData rock =
+      IconData(0xf255, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData paper =
+      IconData(0xf256, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData scissors =
+      IconData(0xf257, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
