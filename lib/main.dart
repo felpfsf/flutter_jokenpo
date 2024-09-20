fe37b30a552118b13946090ff6dc51cdaf97bb83
Felipe Ferreira <feferreira.sf@gmail.com>
2024-09-20 15:46:02 -0300

    ✨ with change notifier

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_jokenpo_app/jokenpo_icons.dart';
+import 'package:provider/provider.dart';
 
+// Main
 void main() {
   runApp(const JokenpoApp());
 }
 
+// APP
 class JokenpoApp extends StatelessWidget {
   const JokenpoApp({super.key});
 
@@ -22,6 +25,8 @@ class JokenpoApp extends StatelessWidget {
   }
 }
 
+// Home
+
 class JokenpoHome extends StatefulWidget {
   const JokenpoHome({super.key});
 
@@ -31,39 +36,151 @@ class JokenpoHome extends StatefulWidget {
 
 class _JokenpoHomeState extends State<JokenpoHome>
     with SingleTickerProviderStateMixin {
-  final ValueNotifier<String> _messageNotifier =
-      ValueNotifier('Faça sua jogada!');
-  final ValueNotifier<IconData?> _pcChoiceImageNotifier = ValueNotifier(null);
-  final ValueNotifier<IconData?> _userChoiceImageNotifier = ValueNotifier(null);
-  final ValueNotifier<bool> _showChoiceNotifier = ValueNotifier(false);
-  final ValueNotifier<bool> _isAnimatingNotifier = ValueNotifier(false);
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => JokenpoController(tickerProvider: this),
+      child: Scaffold(
+        body: Consumer<JokenpoController>(
+          builder: (context, controller, child) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Jo Ken po',
+                  style: TextStyle(fontSize: 30),
+                ),
+                const SizedBox(height: 20),
+                AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 500),
+                  child:
+                      controller.showChoice && controller.pcChoiceImage != null
+                          ? Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Column(
+                                  children: [
+                                    Icon(
+                                      controller.pcChoiceImage,
+                                      size: 80,
+                                    ),
+                                    const Text('Minha escolha'),
+                                  ],
+                                ),
+                                const Text('X'),
+                                Column(
+                                  children: [
+                                    Icon(
+                                      controller.userChoiceImage,
+                                      size: 80,
+                                    ),
+                                    const Text('Sua escolha'),
+                                  ],
+                                ),
+                              ],
+                            )
+                          : RotationTransition(
+                              turns: controller.controller,
+                              child: Image.asset(
+                                'assets/images/rock-paper-scissors.png',
+                                width: 100,
+                                height: 100,
+                              ),
+                            ),
+                ),
+                const SizedBox(height: 60),
+                Text(
+                  controller.message,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 60),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        InkWell(
+                          onTap: () => controller.userChoice('rock'),
+                          child: const Icon(
+                            JokenpoIcons.rock,
+                            size: 50,
+                          ),
+                        ),
+                        const Text('Pedra'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        InkWell(
+                          onTap: () => controller.userChoice('paper'),
+                          child: const Icon(
+                            JokenpoIcons.paper,
+                            size: 50,
+                          ),
+                        ),
+                        const Text('Papel'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        InkWell(
+                          onTap: () => controller.userChoice('scissors'),
+                          child: const Icon(
+                            JokenpoIcons.scissors,
+                            size: 50,
+                          ),
+                        ),
+                        const Text('Tesoura'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class JokenpoController extends ChangeNotifier {
+  String _message = 'Faça sua jogada!';
+  IconData? _pcChoiceImage;
+  IconData? _userChoiceImage;
+  bool _showChoice = false;
+  bool _isAnimating = false;
   int _playCount = 0;
+
   late AnimationController _controller;
 
-  @override
-  void initState() {
+  String get message => _message;
+  IconData? get pcChoiceImage => _pcChoiceImage;
+  IconData? get userChoiceImage => _userChoiceImage;
+  bool get showChoice => _showChoice;
+  bool get isAnimating => _isAnimating;
+  AnimationController get controller => _controller;
+
+  JokenpoController({required TickerProvider tickerProvider}) {
     _controller = AnimationController(
-      vsync: this,
+      vsync: tickerProvider,
       duration: const Duration(milliseconds: 800),
     )..stop();
-    super.initState();
   }
 
-  @override
-  void dispose() {
+  void disposeController() {
     _controller.dispose();
-    _messageNotifier.dispose();
-    _pcChoiceImageNotifier.dispose();
-    _userChoiceImageNotifier.dispose();
-    _showChoiceNotifier.dispose();
-    _isAnimatingNotifier.dispose();
-    super.dispose();
   }
 
-  void _userChoice(String userChoice) {
+  void userChoice(String userChoice) {
     List<String> choices = ['rock', 'paper', 'scissors'];
-    int rdnNum = Random().nextInt(3);
-    String pcChoice = choices[rdnNum];
+    int randomNum = Random().nextInt(3);
+    String pcChoice = choices[randomNum];
 
     final Map<String, IconData> images = {
       'rock': JokenpoIcons.rock,
@@ -71,148 +188,35 @@ class _JokenpoHomeState extends State<JokenpoHome>
       'scissors': JokenpoIcons.scissors,
     };
 
-    _showChoiceNotifier.value = false;
-    _messageNotifier.value = 'Estou escolhendo...';
+    _showChoice = false;
+    _message = 'Estou escolhendo...';
     _playCount++;
-    _userChoiceImageNotifier.value = images[userChoice];
+    _userChoiceImage = images[userChoice];
+    _controller.repeat();
 
     if (_playCount > 1) {
-      _controller.repeat();
-      _isAnimatingNotifier.value = true;
+      _isAnimating = true;
+      notifyListeners();
     }
 
     Future.delayed(const Duration(milliseconds: 1000), () {
-      _pcChoiceImageNotifier.value = images[pcChoice];
-      _showChoiceNotifier.value = true;
-      _isAnimatingNotifier.value = false;
-      _controller.repeat();
+      _pcChoiceImage = images[pcChoice];
+      _showChoice = true;
+      _isAnimating = false;
+      _controller.stop();
+      notifyListeners();
 
-      setState(() {
-        if ((userChoice == 'rock' && pcChoice == 'scissors') ||
-            (userChoice == 'scissors' && pcChoice == 'paper') ||
-            (userChoice == 'paper' && pcChoice == 'rock')) {
-          _messageNotifier.value = "Ah não, voce ganhou! :(";
-        } else if (userChoice == pcChoice) {
-          _messageNotifier.value = "Sem graça, deu empate!";
-        } else {
-          _messageNotifier.value = "Uuhl, voce perdeu! XD";
-        }
-      });
+      if ((userChoice == 'rock' && pcChoice == 'scissors') ||
+          (userChoice == 'scissors' && pcChoice == 'paper') ||
+          (userChoice == 'paper' && pcChoice == 'rock')) {
+        _message = "Ah não, você ganhou! :(";
+      } else if (userChoice == pcChoice) {
+        _message = "Sem graça, deu empate!";
+      } else {
+        _message = "Uuhl, você perdeu! XD";
+      }
+
+      notifyListeners();
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text(
-            'Jo ken po',
-            style: TextStyle(fontSize: 30),
-          ),
-          const SizedBox(height: 20),
-          ValueListenableBuilder(
-            valueListenable: _showChoiceNotifier,
-            builder: (context, showChoice, child) {
-              return AnimatedSwitcher(
-                duration: const Duration(milliseconds: 500),
-                child: showChoice && _pcChoiceImageNotifier.value != null
-                    ? Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Column(
-                            children: [
-                              Icon(
-                                _pcChoiceImageNotifier.value,
-                                size: 80,
-                              ),
-                              const Text('Minha escolha')
-                            ],
-                          ),
-                          const Text('X'),
-                          Column(
-                            children: [
-                              Icon(
-                                _userChoiceImageNotifier.value,
-                                size: 80,
-                              ),
-                              const Text('Sua escolha')
-                            ],
-                          )
-                        ],
-                      )
-                    : RotationTransition(
-                        key: const ValueKey('image'),
-                        turns: _controller,
-                        child: Image.asset(
-                          'assets/images/rock-paper-scissors.png',
-                          width: 100,
-                          height: 100,
-                        ),
-                      ),
-              );
-            },
-          ),
-          const SizedBox(height: 60),
-          ValueListenableBuilder(
-            valueListenable: _messageNotifier,
-            builder: (context, message, child) {
-              return Text(
-                message,
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
-              );
-            },
-          ),
-          const SizedBox(height: 60),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () => _userChoice('rock'),
-                    child: const Icon(
-                      JokenpoIcons.rock,
-                      size: 50,
-                    ),
-                  ),
-                  const Text('Rock')
-                ],
-              ),
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () => _userChoice('paper'),
-                    child: const Icon(
-                      JokenpoIcons.paper,
-                      size: 50,
-                    ),
-                  ),
-                  const Text('Paper')
-                ],
-              ),
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () => _userChoice('scissors'),
-                    child: const Icon(
-                      JokenpoIcons.scissors,
-                      size: 50,
-                    ),
-                  ),
-                  const Text('Scissors')
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
   }
 }
